d014b4ba09a1755c89c18807d763000207246f49
Evgeniy <zhenya_mahnach@mail.ru>
2021-11-26 03:07:52 +0300

    lesson1

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const goods = [
   { title: 'Shirt', price: 150 },
   { title: 'Socks', price: 50 },
   { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+  { title: 'Denim', price: 200 },
+  { title: 'Shoes', price: 120 },
+  { title: 'Bags', price: 230 },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
+const renderGoodsItem = ({ title = 'Title', price = 0 }) =>
+  ` <div class="goods-item">
+    <h3>${title}</h3>
+    <p>${price}</p>
+    <button class="goods-add-btn" type="button">Добавить</button>
+    </div>
+`
 
 const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+  let goodsList = list.map(item => renderGoodsItem(item));
+  goodsList = goodsList.join('');
   document.querySelector('.goods-list').innerHTML = goodsList;
+
 }
 
 
