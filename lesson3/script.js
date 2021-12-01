41b7d868dfc8b322bc73d4e6f2ed5a9cd8c3d2b5
Evgeniy <zhenya_mahnach@mail.ru>
2021-12-02 02:48:11 +0300

    lesson3 rebuild

--- lesson3/script.js
+++ lesson3/script.js
@@ -7,6 +7,35 @@ const goods = [
   { title: 'Bags', price: 230 },
 ];
 
+const reformData = (items) => {
+  return items.map(({ product_name, ...rest }) => {
+    return {
+      ...rest,
+      title: product_name
+    }
+  })
+}
+
+const URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+const GOODS_POSTFIX = "/catalogData.json";
+const BASKET_GOODS_POSTFIX = "/getBasket.json";
+const ADD_GOOD_TO_BASKET_POSTFIX = "/addToBasket.json";
+const DELETE_GOOD_TO_BASKET_POSTFIX = "/deleteFromBasket.json";
+
+
+
+const service = function (url, postfix, method = "GET") {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}${postfix}`, {
+      method
+    }).then((res) => {
+      return res.json();
+    }).then((data) => {
+      resolve(data)
+    })
+  });
+}
+
 class GoodsItem {
   constructor({ title, price }) {
     this.title = title;
@@ -23,33 +52,48 @@ class GoodsItem {
 }
 
 class GoodsList {
-  constructor() {
-    this.goods = goods;
-  }
+
   getSum() {
     return this.goods.reduce((prev, { price }) => prev + price, 0)
   }
-  render() {
-    const _goods = [...this.goods];
+  // Добавление товара в корзину
+  addGoodToBasket() {
+    return service(URL, ADD_GOOD_TO_BASKET_POSTFIX, "POST").then((data) => {
+    });
+  }
 
-    const _goodsItems = _goods.map((item) => {
-      const goodsItem = new GoodsItem(item);
-      return goodsItem.render();
-    })
-    document.querySelector('.goods-list').innerHTML = _goodsItems.join('');
+  setGoods() {
+    return service(URL, GOODS_POSTFIX).then((data) => {
+      return reformData(data)
+    });
+  }
+
+  render() {
+    this.setGoods().then((data) => {
+      this.goods = data;
+      const _goods = [...this.goods];
+      const _goodsItems = _goods.map((item) => {
+        const goodsItem = new GoodsItem(item);
+        return goodsItem.render();
+      })
+      document.querySelector('.goods-list').innerHTML = _goodsItems.join('');
+    });
   }
 }
 
 class Basket {
-  // Добавление товара в корзину
-  addToBasket() { }
-
+  setGoods() {
+    return service(URL, BASKET_GOODS_POSTFIX).then((data) => {
+      this.goods = reformData(data.contents);
+    });
+  }
   // Удаление товара из корзины
-  deleteFromBasket() { }
-
+  deleteGoodBasket(id) {
+    return service(URL, `${DELETE_GOOD_TO_BASKET_POSTFIX}/${id}`, "DELETE").then((data) => {
+    });
+  }
   // Считаем стоимость и количество товаров в корзине
   calcBasket() { }
-
   // Рендер содержимого корзины
   render() { }
 }
